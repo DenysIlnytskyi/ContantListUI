36379ca9e219c1f35840a708ee1510aa789cf94e
DenysIlnytskyi <380984511271k@gmail.com>
2022-08-16 20:00:47 +0200

    Initial Commit

---
+++ ContantListUI/ContantListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContantListUIApp.swift
+//  ContantListUI
+//
+//  Created by Денис Ільницький on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ContantListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
